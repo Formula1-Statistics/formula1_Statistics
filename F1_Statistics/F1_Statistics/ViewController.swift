e9414e19c568da27d0e6c2f8b803304501c28a7d
Fabian Kopf <20180025@students.htl-perg.ac.at>
2022-01-13 12:09:56 +0100

    Create a XCode Project
    Created a project

---
+++ F1_Statistics/F1_Statistics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  F1_Statistics
+//
+//  Created by Fabian Kopf on 13.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
