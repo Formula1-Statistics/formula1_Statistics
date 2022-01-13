b425094ab9ce1c7fbd0ef26e0d75e9a34f9127b3
Fabian Kopf <20180025@students.htl-perg.ac.at>
2022-01-13 13:20:54 +0100

    Created Model and added Storyboard

---
+++ F1_Statistics/F1_Statistics/Driver_Model.swift
@@ -0,0 +1,19 @@
+//
+//  Driver_Model.swift
+//  F1_Statistics
+//
+//  Created by Fabian Kopf on 13.01.22.
+//
+
+import Foundation
+import CoreText
+
+class Driver_Model{
+    var nameDriver = ""
+    let birthDate = ""
+    var nation = ""
+    var team = ""
+    var points = 0
+    
+    
+}
