45f036f58bfad6266d1b6d1ea181404d25432039
Fabian Kopf <20180025@students.htl-perg.ac.at>
2022-02-03 12:44:35 +0100

    Detail View

--- F1_Statistics/F1_Statistics/ViewController.swift
+++ F1_Statistics/F1_Statistics/ViewController.swift
@@ -14,9 +14,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("ViewController")
         nameLabel.text = driver?.nameDriver
-        
+        teamLabel.text = driver?.team
+        nationLabel.text = driver?.nation
+        // numberLabel.text = "\(driver?.driverNumber)"
+        birthdateLabel.text = driver?.birthDate
+        pointsLabel.text = driver?.points
         // Do any additional setup after loading the view.
     }
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var teamLabel: UILabel!
+    @IBOutlet weak var nationLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var birthdateLabel: UILabel!
+    @IBOutlet weak var pointsLabel: UILabel!
 }
 
